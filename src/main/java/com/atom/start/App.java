6f691a755d08092a78c7eab7039f4a5b36a6eec4
zitingxiang <1592146206>
2016-10-24 15:28:02 +0800

    initial commit

---
+++ src/main/java/com/atom/start/App.java
@@ -0,0 +1,13 @@
+package com.atom.start;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
